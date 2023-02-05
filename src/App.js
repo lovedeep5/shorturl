08a56d91f80c99903e9f5a1fbf14939c891f7888
lovedeep5 <lovedeep5.abh@gmail.com>
2023-02-06 00:46:13 +0530

    config change

--- src/App.js
+++ src/App.js
@@ -1,18 +1,15 @@
 import "./App.css";
 import URLShortnerForm from "./Components/URLShortnerForm/";
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
-import Redirect from "./Components/Redirect";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Redirect from "./Components/Redirect/";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Navigate to="/shorturl" />}  />
-            
-            
-          <Route path="/shorturl" element={<URLShortnerForm />} />
-          <Route path="/shorturl/:id" element={<Redirect />} />
+          <Route path="/" element={<URLShortnerForm />} />
+          <Route path="/:id" element={<Redirect />} />
         </Routes>
       </BrowserRouter>
     </div>
--- src/helpers/getCurrentDomain.js
+++ src/helpers/getCurrentDomain.js
@@ -1,5 +1,5 @@
 const currentDomain = window.location.hostname;
 const currentProtocol = window.location.protocol;
-const SITE_URL = `${currentProtocol}//${currentDomain}/shorturl`;
+const SITE_URL = `${currentProtocol}//${currentDomain}/`;
 
 export default SITE_URL;
